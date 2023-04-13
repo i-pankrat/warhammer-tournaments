c8f68b876eaf5464bcf87d35eeaf2a849b345e62
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-04-13 21:03:19 +0300

    Update tournament page
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- WarhammerTournaments/WarhammerTournaments/Controllers/TournamentsController.cs
+++ WarhammerTournaments/WarhammerTournaments/Controllers/TournamentsController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using WarhammerTournaments.Interfaces;
 
 namespace WarhammerTournaments.Controllers;
 
 public class TournamentsController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly ITournamentRepository _tournamentRepository;
+
+    public TournamentsController(ITournamentRepository tournamentRepository)
     {
-        return View();
+        _tournamentRepository = tournamentRepository;
+    }
+
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        var tournaments = await _tournamentRepository.GetAll();
+        return View(tournaments);
     }
 }
