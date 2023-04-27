34975d10bfa77f7bfd996e14e1e3183ad3cbb7e2
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-04-27 14:55:37 +0300

    Create join to the tournament page
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

---
+++ WarhammerTournaments/WarhammerTournaments/ViewModels/JoinViewModel.cs
@@ -0,0 +1,8 @@
+namespace WarhammerTournaments.Models;
+
+public class JoinViewModel
+{
+    public string UserId { get; set; }
+    public string Fraction { get; set; }
+    public string Roster { get; set; }
+}
